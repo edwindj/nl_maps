d76a30f6f3521695cc75fe9f93f141641c9e8940
Edwin de Jonge <edwindjonge@gmail.com>
2017-04-10 21:28:25 +0200

    updating script

---
+++ create_region.bash
@@ -0,0 +1,27 @@
+mkdir -p rd
+mkdir -p wgs84
+
+REGIONNAME="gemeente"
+
+for REGIONNAME in "gemeente" "coropgebied" "provincie"
+do
+  for YEAR in {2003..2016}
+  do 
+    REGION="${REGIONNAME}_${YEAR}"
+    #echo $REGION
+
+    # get WGS84 (EPSG:4326)
+    curl "http://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_${REGION}_gegeneraliseerd&outputFormat=json&SRSName=urn:x-ogc:def:crs:EPSG:4326" > "wgs84/${REGION}.json"
+    mapshaper "wgs84/$REGION.json" -simplify 10% -o "wgs84/$REGION.geojson"
+    mapshaper "wgs84/$REGION.json" -simplify 10% -o "wgs84/$REGION.topojson"
+
+    # get rijkdriehoeksstelsel (EPSG:28894)
+    # get WGS84 (EPSG:4326)
+    curl "http://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_${REGION}_gegeneraliseerd&outputFormat=json" > "rd/${REGION}.json"
+    mapshaper "rd/$REGION.json" -simplify 10% -o "rd/$REGION.geojson"
+    mapshaper "rd/$REGION.json" -simplify 10% -o "rd/$REGION.topojson"
+  done
+done
+
+# remove all original files
+rm */*.json
